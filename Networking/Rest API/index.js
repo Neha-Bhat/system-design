457bc3ffc9892275957c14232d09a5f01be80371
Neha <nehabhat2209@gmail.com>
2025-08-20 23:06:42 +0530

    GraphQL server setup

--- Networking/Rest API/index.js
+++ Networking/Rest API/index.js
@@ -2,10 +2,35 @@ import  express  from 'express';
 const app = express();
 const PORT = 2703;
 
+const todos = [
+    {
+        id: "1",
+        title: "Task1",
+        status: "In Progress"
+    },
+    {
+        id: "2",
+        title: "Task2",
+        status: "Done"
+    }
+]
+
 app.all('/', (req, res) => {
     console.log("Request: ",req);
     console.log("Response: ",res);
     res.send("I'm up!!")
+})
+
+app.get('/todos', (req,res) => {
+    res.json(todos);
+})
+
+app.post('/todos', (req, res) => {
+    const newTodo = req.body;
+    todos.push(newTodo)
+    res.json({
+        message: 'New task added!'
+    })
 })
 
 app.listen(PORT, () => {
